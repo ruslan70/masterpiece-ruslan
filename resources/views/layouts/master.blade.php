05fe06f9ef6b432aa4f1234d3c7b4f5437e6f97a
ruslan70 <ruslan.khatuev70@gmail.com>
2022-08-24 16:48:08 +0200

    new build

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
     <script src="{{ asset('/js/app.js') }}" defer></script>
 
 
