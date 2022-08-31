052496c51eed24d9b5e9ae52efdf1f7974dec100
ruslan70 <ruslan.khatuev70@gmail.com>
2022-08-31 10:00:39 +0200

    change forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,49 +17,55 @@
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden text-center">
-        <form method="POST" action="{{ route('login') }}">
+    <!-- <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden text-center"> -->
+    <div class="w-full max-w-xs text-center">
+        <form method="POST" action="{{ route('login') }}" class="shadow-md rounded px-8 pt-6 pb-8 mb-4">
             @csrf
 
             <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+            <div class="mb-4">
+                <x-label class="block text-gray-700 text-sm font-bold mb-2" for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                                type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            <div class="mb-6">
+                <x-label class="block text-gray-700 text-sm font-bold mb-2" for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input id="password" class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
+                <p class="text-red-500 text-xs italic">Please choose a password.</p>
             </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
+            
+
+            <!-- Forgot your password? -->
+            <div class="flex items-center justify-between">
+                @if (Route::has('password.request'))
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <!-- Remember Me -->
+            <div class="block mb-2 mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mb-4 mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
             
-    </div>
-                <x-button class="ml-3 bg-primary">
+    
+                <x-button class="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
-    
+    </div>
     </x-auth-card>
 </x-guest-layout>
 @endsection
